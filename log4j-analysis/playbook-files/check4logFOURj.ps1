460077e5d66395272be4193528bcdb7f8607b629
Tobias Michalski <humpalum@users.noreply.github.com>
2021-12-16 11:13:39 +0100

    Check all Drives with the filesystem checks

--- log4j-analysis/playbook-files/check4logFOURj.ps1
+++ log4j-analysis/playbook-files/check4logFOURj.ps1
@@ -3,7 +3,17 @@ $ErrorActionPreference= 'silentlycontinue'
 $outdir = "results"
 $out = -join($outdir, "\",$env:COMPUTERNAME,"_check4logFOURj_",$date)
 mkdir $outdir 
-gci 'C:\' -rec -force -include *.jar | foreach {select-string "JndiLookup.class" $_} | select -exp Path | out-file "$out.win.jndiclass"
+
 handle | Select-String "log4j" | out-file "$out.win.openfiles"
-dir -Recurse C:\ |Select-Object FullName | Select-String -Pattern "log4j" | out-file "$out.win.fsfiles"
+
+foreach ($drive in gdr -PSProvider 'FileSystem' |  select -exp Root)
+{
+    dir -Recurse $drive |Select-Object FullName | Select-String -Pattern "log4j" | out-file "$out.win.fsfiles"
+}
+
 Get-WmiObject Win32_Process | Select-Object CommandLine | Select-String "log4j" | out-file "$out.win.processes"
+
+foreach ($drive in gdr -PSProvider 'FileSystem' |  select -exp Root)
+{
+    gci $drive -rec -force -include *.jar | foreach {select-string "JndiLookup.class" $_} | select -exp Path | out-file "$out.win.jndiclass"
+}
