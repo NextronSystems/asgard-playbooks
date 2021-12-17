7885fc3dcda16d8b41c620c6f87221a64a14a231
phantinuss <79651203+phantinuss@users.noreply.github.com>
2021-12-17 10:55:51 +0100

    revert to showing log4j 2.15 as high

--- log4j-analysis/evaluation-script/evalPB.py
+++ log4j-analysis/evaluation-script/evalPB.py
@@ -29,8 +29,8 @@ def main():
         print (exc.message, '\n', exc.argument)
         return 1
 
-    #Match for these: log4j-api-2.12.1.jar - all versions from 2.0.0 to 2.14.9
-    log4jre = re.compile(".+log4j-.{0,10}(2\.([0-9]\.|1[0-4])\.[0-9]+).*")
+    #Match for these: log4j-api-2.12.1.jar - all versions from 2.0.0 to 2.15.*
+    log4jre = re.compile(".+log4j-.{0,10}(2\.([0-9]\.|1[0-5])\.[0-9]+).*")
 
     # Setup Temporary directory
     try: 
